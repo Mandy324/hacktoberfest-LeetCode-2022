33695af3863df6fe3b5e11283820b2f7638b4f57
MaheshDoiphode <58901842+MaheshDoiphode@users.noreply.github.com>
2022-09-28 00:07:23 +0530

    Add files via upload

---
+++ Java/1.Two_Sum.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+/*
+1. Two Sum
+Easy
+
+38190
+
+1221
+
+Add to List
+
+Share
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+*/
+
+// Approach 1. HashMap method
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(tracker.containsKey(nums[i])){
+                int left = tracker.get(nums[i]);
+                return new int[]{left, i};
+            }else
+                tracker.put(target - nums[i], i);
+        }
+        return new int[2];
+    }
+}
