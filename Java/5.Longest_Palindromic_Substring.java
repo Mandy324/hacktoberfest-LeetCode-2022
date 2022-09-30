1e6ccecde323e6dc3f94f2b45c2315ce565a930b
Mandy324 <83758696+Mandy324@users.noreply.github.com>
2022-10-01 03:26:29 +0530

    Add files via upload

---
+++ Java/5.Longest_Palindromic_Substring.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+/* 
+
+5. Longest Palindromic Substring
+Medium
+
+21771
+
+1253
+
+Add to List
+
+Share
+Given a string s, return the longest palindromic substring in s.
+
+A string is called a palindrome string if the reverse of that string is the same as the original string.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+
+*/
+
+public class Solution {
+    private int lo, maxLen;
+    
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+        
+        for (int i = 0; i < len-1; i++) {
+             extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+             extendPalindrome(s, i, i+1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+    
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }}
